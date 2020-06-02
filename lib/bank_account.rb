0df4de276b4463aa0e40d1d65884b926bed2630a
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-06-02 02:57:48 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,15 @@
 class BankAccount
 
+  def initialize(name)
+    @name = name
+
+
+  def initialize(status)
+    @status = open
+  end
+
+  def display_balance(balance)
+    display_balance = balance
+  end
+
 end
